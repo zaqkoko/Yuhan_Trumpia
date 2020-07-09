802b1de8d10694eec0a127ce401a50888dd10b47
zaqkoko <zaqkoko@naver.com>
2020-07-10 08:59:38 +0900

    1

---
+++ send.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title></title>
+</head>
+<body>
+  <form>
+    <p>메세지 보내기</p>
+    <br />
+    <textarea name="sms_text" cols="40" rows="30"></textarea>
+    <div style="font-size: 12px; padding-top: 3px; padding-bottom: 10px;">
+      <span class="bold">글자 수 제한</span>&nbsp;0/150
+    </div>
+    <input type="submit" value="보내기" />
+  </form>
+</body>
+</html>
