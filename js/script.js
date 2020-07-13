85e5f49e6273c3b93bee98804c6da757fd183593
zaqkoko <zaqkoko@naver.com>
2020-07-13 09:45:08 +0900

    DELETE

--- js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $("#gnb > li").mouseenter(function(){
-    $(this).children('.sub').addClass('active');
-  });
-  $("#gnb > li").mouseleave(function(){
-    $(this).children('.sub').removeClass('active');
-  });
-});
