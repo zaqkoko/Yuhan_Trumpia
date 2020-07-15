094e578e8c6a3b49660860f3fb987ff103fb7927
lsh <keshes@naver.com>
2020-07-15 17:57:24 +0900

    20200715

--- board/test.php
+++ board/test.php
@@ -5,15 +5,14 @@
 	<title>board</title>
 
 
-  <link rel="stylesheet" href="board_css.css">
+  <link rel="stylesheet" href="board/board_css.css">
 	<script src="http://code.jquery.com/jquery-latest.js"></script>
   <script type="text/javascript">
   //가장 마지막에 실행 (위에서부터 아래로 html 태그들이 실행된 후 document.ready()를 실행)
   $(document).ready(function(){
     //새로고침 확인
-    alert("sdaddsadsa");
+    alert("초코파zzzzzzzzzzzzz");
 
-//type값(1 or 2)에 따라 발송완료와 발송예약을 표시
     //type클래스를 가진 태그를 반복해서 가져와 함수를 실행
     $(".type").each(function(){
       //가져오는 값 확인용
@@ -36,10 +35,14 @@
     });
 
 
+    //전송내역 건수 출력
+    $("span").append(
+      //건수를 출력하는 php파일 include
+        <?php include "board/send_type_value.php"; ?>
+    );
 
 
 
-//전체 체크
     //전체 체크박스를 클릭했을 때
     $("#allCheck").click(function(){
       //전체체크박스가 true 상태라면
@@ -52,14 +55,14 @@
         //전체 체크박스가 false 상태라면
       }else{
         //하위 체크박스를 반복해서 불러와 함수를 실행
-        $(".checkbox").each(function(){
+        $(".checkbox").each(function(i,checkbox){
           //현재 하위체크박스 상태를 false로 변경
           $(this).prop("checked",false);
         });
       }
     });
 
-//이게 최선인가? 더 보완하셈
+
 //전체체크 상태에서 하위 체크박스가 하나라도 false일 때 전체 체크박스 상태를 false로 바꿈
     //하위 체크박스를 클릭했을 때
       $(".checkbox").click(function() {
@@ -82,14 +85,12 @@
           //post방식으로 전송 (get,post 둘다 상관없음)
           type:"POST",
           //AllDelete.php를 호출
-          url:"AllDelete.php",
+          url:"board/AllDelete.php",
           //성공했을 때 함수 실행
           success:function(data){
-						//data를 통해 리턴 받아 재출력하려 했으나 아직 수정중
+
             //체크박스 상위요소 tr태그를 지움
             $(".checkbox").parents("tr").remove();
-						//전송내역 건수를 0으로 출력
-						$("span").text("0");
           },
           //에러가 생겼을 때 함수를 실행
           error:function(){
@@ -121,16 +122,14 @@
               //post방식으로 전송 (get,post 둘다 상관없음)
               type:"POST",
               //delete.php를 호출
-              url:"delete.php",
+              url:"board/delete.php",
               //t값을 가진 val이라는 데이터를 전송
               data: {val:t},
               //성공했을 때 함수실행
               success:function(data){
-   							//data를 통해 리턴 받아 재출력하려 했으나 아직 수정중
+
                 //t값을 가진 체크박스 상위요소 tr 태그를 지움
                 $("input:checkbox[value="+t+"]").parents("tr").remove();
-
-
               },
               //에러가 생겼을 때 함수를 실행
               error:function(){
@@ -169,7 +168,7 @@
 		<button id="delete">삭제</button>
 		<div id="dvar">
       	<!-- span안에 현재 전송된 데이터값을 출력함 -->
-			<p>전송내역 <span>  <?php include "send_type_value.php"; ?></span>건</p>
+			<p>전송내역 <span></span>건</p>
 			<hr>
 		</div>
 	</div>
@@ -183,7 +182,7 @@
       <th width="10%"><p>발송상태</p></th>
 			<tr id="dcell">
         	<!-- 데이터 출력 -->
-            <?php include "select_sms.php" ?>
+            <?php include "board/select_sms.php" ?>
 
 
 
@@ -192,12 +191,12 @@
 		</table>
 		<!-- 메뉴바 -->
 	<div class="menu">
-		<img src="../img/sms2.png">
-		<img src="../img/cal2.png">
-		<img src="../img/hi.png">
-		<img src="../img/ad2.png">
+		<img src="img/sms2.png">
+		<img src="img/cal2.png">
+		<img src="img/hi.png">
+		<img src="img/ad2.png">
 	</div>
-	<img id="cht" src="../img/cht.png">
+	<img id="cht" src="img/cht.png">
 
 
 </body>
