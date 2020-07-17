81b09fddd4b7fa1b1f63481997ec1de1aace9b90
zaqkoko <zaqkoko@naver.com>
2020-07-18 01:00:12 +0900

    200718

--- send/send.php
+++ send/send.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0" /> -->
     <title>ToySend</title>
     <script src="//code.jquery.com/jquery-3.3.1.min.js"></script>
 
