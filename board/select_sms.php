7e12b3f9ccf69f93a869bdae2c60b0e5275537f4
cyong9573 <cyong9573@naver.com>
2020-07-20 16:25:37 +0900

    200720 수화 수정

--- board/select_sms.php
+++ board/select_sms.php
@@ -11,7 +11,7 @@ $r = mysqli_query($conn, $q);
 while ($row = mysqli_fetch_array($r)) {
   //echo로 html 테이블 태그를 포함한 문자열과 출력할 데이터를 문자열로 작성
   echo "<tr>
-    <td width='5%'><input type='checkbox' class='checkbox' name='list' value='" . $row['send_id'] . "'></td>
+    <td width='5%'><input type='checkbox' class='checkbox' onclick='check();' value='" . $row['send_id'] . "'></td>
     <td width='20%'>" . $row['receiver'] . "</td>
     <td width='40%'>" . $row['send_message'] . "</td>
     <td width='20%'>" . $row['send_time'] . "</td>
