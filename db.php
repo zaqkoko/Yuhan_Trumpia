5d0945e0a1944a22a9cfe003f5051cca5799cd30
cyong9573 <cyong9573@naver.com>
2020-07-20 14:02:23 +0900

    db 수정

--- db.php
+++ db.php
@@ -2,14 +2,7 @@
 
 session_start();
 
-// 만약 세션의 name의 값이 ""라면
-if ($_SESSION['name'] == "") {
-    // $name의 값은 세션 id값으로 지정
-    $name = $_SESSION['id'];
-} else {
-    // $name의 값이 있다면 name은 세션 name값으로 지정
-    $name = $_SESSION['name'];
-}
+$name = $_SESSION['id'];
 
 
 // mysqli_connect = php에서 mysql을 연결해주는 함수 (반대는 mysqli_close)
