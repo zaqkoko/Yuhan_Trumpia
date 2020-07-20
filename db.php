dcf278c61d2e2fdd748537898c6a86179725d7c4
zaqkoko <zaqkoko@naver.com>
2020-07-20 15:17:01 +0900

    200720

--- db.php
+++ db.php
@@ -1,16 +1,6 @@
 <?php
-<<<<<<< HEAD
-    $conn = mysqli_connect("localhost", "root", "04540121", "toy") or die("sql접속ㄴㄴ");
-=======
 
-    $con=mysqli_connect("localhost", "root", "04540121", "toy") or die("sql접속ㄴㄴ");
+$name = $_SESSION['id'];
 
-    session_start();
-
-    $name = $_SESSION['id'];
-
-    // mysqli_connect = php에서 mysql을 연결해주는 함수 (반대는 mysqli_close)
-    $conn = mysqli_connect("localhost", "root", "04540121", "toy");
-
->>>>>>> f7050218bbf17b53c007a0d9a34b041ac112a60e
-?>
+// mysqli_connect = php에서 mysql을 연결해주는 함수 (반대는 mysqli_close)
+$conn = mysqli_connect("localhost", "root", "04540121", "toy");
