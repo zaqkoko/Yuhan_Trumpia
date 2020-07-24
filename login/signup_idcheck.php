0585aaf1eff764db349eadbbe6a5a393291adb6c
zaqkoko <zaqkoko@naver.com>
2020-07-24 10:05:36 +0900

    200724 modify

--- login/signup_idcheck.php
+++ login/signup_idcheck.php
@@ -4,13 +4,13 @@ include "../db.php";
 //signup.php에서 입력받은 id를 $id로 저장
 $id = $_POST["id"];
 //입력한 id값과 같은 데이터 가져옴
-$sql = "select COUNT(*) from user where id='$id'";
+$sql = "select * from user where id='$id'";
 //쿼리실행
 $ret = mysqli_query($conn, $sql);
 //받은 결과값을 $row에 array로 저장
 $row = mysqli_fetch_array($ret);
 //$row의 값이 null일때 (결과가 없을 때)
-if ($row >= 0) {
+if ($row == 0) {
     //1을 출력
     echo "1";
 }
