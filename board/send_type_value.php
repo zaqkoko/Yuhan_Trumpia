88444e0b6fe4cf79942a8608dad150152dd2848f
cyong9573 <cyong9573@naver.com>
2020-07-24 12:08:59 +0900

    200724 board 내림정렬로 출력

--- board/send_type_value.php
+++ board/send_type_value.php
@@ -4,7 +4,7 @@
 include "../db.php";
 
 //유저아이디와 동일하고 send_type이 전송된 메시지 타입(1)만 가져온다.
-$q = "SELECT * FROM sms WHERE send_type='1' AND user_id='$name'";
+$q = "SELECT * FROM sms WHERE send_type='1' AND user_id='$name' ORDER BY `sms`.`send_id` DESC";
 //r에 연결된 데이터베이스에 쿼리문 q를 실행한 값을 초기화해 가져온다.
 $r = mysqli_query($conn, $q);
 //mysqli_num_rows()를 통해 행 수를 구해와 출력
